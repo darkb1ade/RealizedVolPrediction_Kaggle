7bc6d3cfeeb1d4d08402921e3399f76400326b0f
Pasin <jobpasin@hotmail.com>
2021-09-12 16:42:30 +0900

    Add main.py to test train_model

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from helper.dataset import DataLoader
 from IPython.display import display
+import pandas as pd
+import numpy as np
+from model.LGBM import LGBMModel
+# dl = DataLoader('train')
+#
+# # getting each stock (sample)
+# d1, _ = dl.get_each_parquet(0)
+#
+# # get groundtruth/target
+# gt = dl.get_gt()
+# print("Ground Truth")
+# display(gt)
+#
+# # getting all stocks
+# df, _ = dl.get_all_parquet()
+# print("All stocks")
+# display(df)
 
-dl = DataLoader('train')
+def main():
+    df = pd.read_csv("data/update8Sep_feature.csv")
+    feature_name = list(df.columns)
+    feature_name.remove("time_id")
+    feature_name.remove("logReturn_price")
+    lgbm_model = LGBMModel(feature_name, "logReturn_price")
+    train_df, test_df = np.split(df, [int(.8*len(df))])
+    test_predictions, score = lgbm_model.train(train_df, test_df)
+    print(f"Test prediction: {test_predictions}, Score: {score}")
 
-# getting each stock (sample)
-d1, _ = dl.get_each_parquet(0)
-
-# get groundtruth/target
-gt = dl.get_gt()
-print("Ground Truth")
-display(gt)
-
-# getting all stocks
-df, _ = dl.get_all_parquet()
-print("All stocks")
-display(df)
+if __name__ == "__main__":
+    main()
