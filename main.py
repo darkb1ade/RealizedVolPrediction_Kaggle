d481dd27ee18a9a9f20d4c4b1f6814815133f33d
darkb1ade <npatamawadee@gmail.com>
2021-09-18 14:53:49 +0900

    Edit main

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ d1 = result dataframe
 d_book_inter, d_trade_inter = intermediate dataframe
 d_book, d_trade = initial dataframe
 '''
-d1, d_book_inter, d_trade_inter,  d_book, d_trade = dl.get_each_parquet(0) # ex: stock_id = 0
+#d1, d_book_inter, d_trade_inter,  d_book, d_trade = dl.get_each_parquet(0) # ex: stock_id = 0
 
 
 # get groundtruth/target
-gt = dl.get_gt()
-print("Ground Truth")
-display(gt)
+#gt = dl.get_gt()
+#print("Ground Truth")
+#display(gt)
 
 # getting all stocks
-df= dl.get_all_parquet()
+print('get all stock')
+df= dl.get_all_parquet(show = True)
 print("All stocks")
 display(df)
+
+df.to_csv('update10Sep_feature.csv',index = False)
 
 
